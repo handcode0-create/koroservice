1629f8474e440fac0cc3b9f22285fbb93b95c70f
handcode0-create <handcode0@gmail.com>
2026-09-21 13:43:56 +0000

    fix(mobile): clean client tab navigation

--- mobile/src/components/app-tabs.tsx
+++ mobile/src/components/app-tabs.tsx
@@ -1,6 +1,5 @@
 import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
-import { Colors } from '@/constants/theme';
 import { useTheme } from '@/hooks/use-theme';
 
 export default function AppTabs() {
